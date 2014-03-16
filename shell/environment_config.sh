3a2595445627a62117a526104afb213f88187f3e
hahaya <hahayacoder@gmail.com>
2014-03-16 09:32:20 +0800

    Update environment_config.sh
    fix some errors.

--- shell/environment_config.sh
+++ shell/environment_config.sh
@@ -84,7 +84,7 @@ if [ -z "$gcc_info" ]; then
     sudo apt-get -y install gcc-4.8
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 
-    sudo apt-get -y install g++4.8
+    sudo apt-get -y install g++-4.8
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50  
 
     echo "gcc finish install."
